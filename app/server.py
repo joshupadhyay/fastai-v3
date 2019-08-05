4cf235e3779ed05850bc22cabf4b668e399eb587
Josh U <47004365+joshupadhyay@users.noreply.github.com>
2019-08-05 16:14:59 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1vlo-9W8DyrOfHWqGXU2yBJJJHljcMal9'
+export_file_name = 'elon_brad_fastai_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Elon', 'Brad']
 path = Path(__file__).parent
 
 app = Starlette()
